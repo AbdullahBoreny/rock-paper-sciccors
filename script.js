0d767e9071562560ca997c10e651a791908eb510
AbdullahBoreny <abdullah55boreny@gmail.com>
2024-09-25 19:06:56 +0300

    adding a tie conditional statment at line 51

--- script.js
+++ script.js
@@ -16,34 +16,48 @@ else {
 }
 
 function getHumanChoice() {
-return prompt("What is your choise?");
+let humanChoice = prompt("What is Your choice?");
+humanChoice = humanChoice.toLowerCase();
+return humanChoice;
 }
 
 function playRound(humanChoice, computerChoice) {
-humanChoice = humanChoice.toLowerCase();
+
 if(humanChoice == "rock" && computerChoice == "paper") {
-    console.log("You lose! Paper beats rock!");
+    console.log("You Lose! Paper beats rock!");
     computerScore++;
 }
 else if(humanChoice == "scissor" && computerChoice == "paper"){
-    console.log("you win scissor beats paper");
+    console.log("You Win! Scissor beats Paper");
     humanScore++;
 }
 else if(humanChoice == "rock" && computerChoice == "scissor"){
-    console.log("you win rock beats scissor");
+    console.log("You Win! rock beats scissor");
     humanScore++;
 }
 else if(humanChoice == "paper" && computerChoice == "rock"){
-    console.log("you win paper beats rock");
+    console.log("You Win! paper beats rock");
     humanScore++;
 }
 else if(humanChoice == "paper" && computerChoice == "scissor"){
-    console.log("you lose scissor beats paper");
+    console.log("You Lose scissor beats paper");
     computerScore++;
 }
+else if(humanChoice == "sicssor" && computerChoice == "rock"){
+    console.log("You Lose rock beats scissor");
+    computerScore++;
+} 
+else if (humanChoice == computerChoice){
+    console.log("TIE!");
+}
+
+
 
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
+
